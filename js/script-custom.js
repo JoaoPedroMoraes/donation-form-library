ebc0f21163d688532f8f6bec6cfebfa8b5e6f5df
JoaoPedroMoraes <joaopedromoraes@outlook.com>
2019-03-30 20:01:52 -0300

    adding new color and navigation bar, and rearranging elements on the front

--- js/script-custom.js
+++ js/script-custom.js
@@ -4,7 +4,7 @@ function rmChild(index){
     a.removeChild(b);
 }
 
-function formFields (size,icon_name, id, mark, example_text){//size,icon_name, id, label, example_text
+function formFields (index, size,icon_name, id, mark, example_text){//size,icon_name, id, label, example_text
     var field = document.createElement('div');
     field.className = "input-field col " + size; //size
 
@@ -14,13 +14,14 @@ function formFields (size,icon_name, id, mark, example_text){//size,icon_name, i
     field.appendChild(icon);
 
     var input = document.createElement('input');
-    input.id = id; //id
+    input.id = id + index; //id
     input.type = 'text';
     input.className = 'validate';
+    input.setAttribute('name','teste'); 
     field.appendChild(input);
 
     var label = document.createElement('label');
-    label.setAttribute('for',id); //label
+    label.setAttribute('for',id + index); //label
     label.innerText = mark;
     field.appendChild(label);
 
@@ -34,14 +35,14 @@ function formFields (size,icon_name, id, mark, example_text){//size,icon_name, i
     return field;
 }
 
-function completeForm(x){
+function completeForm(index){
     var row = document.createElement('div');
     row.className = 'row';
-    row.id = 'form-' + x;
+    row.id = 'form-' + index;
     document.getElementById('formulario').appendChild(row);
 
     var col = document.createElement('div');
-    col.className = 'col s12 m8 offset-m2';
+    col.className = 'col s12 m10 offset-m1 l12';
     row.appendChild(col);
 
     var card = document.createElement('div');
@@ -53,18 +54,18 @@ function completeForm(x){
 
     card.appendChild(form);
 
-    form.appendChild(formFields('s6 l2','plus_one','quantidade','Quantidade','ex. 1'));
-    form.appendChild(formFields('s12 l5','account_circle','autor','Autor','ex: Antoine de Saint-Exupéry'));
-    form.appendChild(formFields('s12 l5','title','titulo','Título','O Pequeno Príncipe'));
-    form.appendChild(formFields('s12 l2','import_contacts','edicao','Edição','ex. 1ª'));
-    form.appendChild(formFields('s6 l2','date_range','ano','Ano','ex: 1945'));
-    form.appendChild(formFields('s12 l5','domain','editora','Editora','ex: Editora Agir'));
-    form.appendChild(formFields('s12 l3','place','local','Local','ex: Editora Agir'));
+    form.appendChild(formFields(index,'s6 m3 l1','plus_one','quantidade','Quantidade','ex. 1'));
+    form.appendChild(formFields(index,'s12 m9 l2','account_circle','autor','Autor','ex: Antoine de Saint-Exupéry'));
+    form.appendChild(formFields(index,'s12 m12 l3','title','titulo','Título','O Pequeno Príncipe'));
+    form.appendChild(formFields(index,'s12 m3 l1','import_contacts','edicao','Edição','ex. 1ª'));
+    form.appendChild(formFields(index,'s6 m3 l1','date_range','ano','Ano','ex: 1945'));
+    form.appendChild(formFields(index,'s12 m6 l2','domain','editora','Editora','ex: Editora Agir'));
+    form.appendChild(formFields(index,'s12 m12 l2','place','local','Local','ex: Editora Agir'));
 
     var button = document.createElement('a')
     button.className = 'waves-effect waves-light btn-large red';
     button.innerText = 'Excluir';
-    button.setAttribute('onclick','rmChild(' + x + ');');
+    button.setAttribute('onclick','rmChild(' + index + ' ); M.toast({html: "Item removido!"});');
 
     card.appendChild(button);
 }

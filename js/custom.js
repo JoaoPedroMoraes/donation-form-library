f3f775f86d1f48261654215bd9ce1c1d0add7b96
João Pedro Moraes <joaopedromoraes@outlook.com>
2019-04-04 10:04:40 -0300

    adding exclude confirmation

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,19 @@
-function rmChild(index){
-    var a  = document.getElementById('formulario');
+function confirmar_exclusao(index) {
+
+    msg = "Tem certeza que deseja excluir este item?";
+    if (confirm(msg)) {
+        rmChild(index);
+        M.toast({ html: "Item removido!" });
+    }
+}
+
+function rmChild(index) {
+    var a = document.getElementById('formulario');
     var b = document.getElementById('form-' + index);
     a.removeChild(b);
 }
 
-function formFields (index, size,icon_name, id, mark, example_text){//size,icon_name, id, label, example_text
+function formFields(index, size, icon_name, id, mark, example_text) {//size,icon_name, id, label, example_text
     var field = document.createElement('div');
     field.className = "input-field col " + size; //size
 
@@ -17,25 +26,25 @@ function formFields (index, size,icon_name, id, mark, example_text){//size,icon_
     input.id = id + index; //id
     input.type = 'text';
     input.className = 'validate';
-    input.setAttribute('name','teste'); 
+    input.setAttribute('name', 'teste');
     field.appendChild(input);
 
     var label = document.createElement('label');
-    label.setAttribute('for',id + index); //label
+    label.setAttribute('for', id + index); //label
     label.innerText = mark;
     field.appendChild(label);
 
     var span = document.createElement('span');
     span.className = 'helper-text';
-    span.setAttribute('data-error','Tá Errado Otário'); 
-    span.setAttribute('data-sucess','Acertô Miseravis');
+    span.setAttribute('data-error', 'Tá Errado Otário');
+    span.setAttribute('data-sucess', 'Acertô Miseravis');
     span.innerText = example_text; //example_text
     field.appendChild(span);
 
     return field;
 }
 
-function addForm(index){
+function addForm(index) {
     var row = document.createElement('li');
     row.id = 'form-' + index;
     document.getElementById('formulario').appendChild(row);
@@ -51,12 +60,12 @@ function addForm(index){
 
     var text = document.createElement('a');
     text.className = 'btn-flat';
-    text.innerHTML = 'Titulo '+index;
+    text.innerHTML = 'Titulo ' + index;
     col.appendChild(text);
 
     var button = document.createElement('a');
-    button.className = 'waves-effect waves-light btn-small red';
-    button.setAttribute('onclick','rmChild(' + index + ' ); M.toast({html: "Item removido!"});');
+    button.className = 'waves-effect waves-light btn-small right red';
+    button.setAttribute('onclick', 'confirmar_exclusao(' + index + ' )');
     button.innerHTML = 'excluir';
     col.appendChild(button);
 
@@ -67,13 +76,13 @@ function addForm(index){
     var span = document.createElement('span');
     form.appendChild(span);
 
-    span.appendChild(formFields(index,'s6 m3 xl1','plus_one','quantidade','Quantidade','ex. 1'));
-    span.appendChild(formFields(index,'s12 m9 xl2','account_circle','autor','Autor','ex: Antoine de Saint-Exupéry'));
-    span.appendChild(formFields(index,'s12 m12 xl3','title','titulo','Título','O Pequeno Príncipe'));
-    span.appendChild(formFields(index,'s12 m3 xl1','import_contacts','edicao','Edição','ex. 1ª'));
-    span.appendChild(formFields(index,'s6 m3 xl1','date_range','ano','Ano','ex: 1945'));
-    span.appendChild(formFields(index,'s12 m6 xl2','domain','editora','Editora','ex: Editora Cultura'));
-    span.appendChild(formFields(index,'s12 m12 xl2','place','local','Local','ex: Brasil'));
+    span.appendChild(formFields(index, 's6 m3 xl1', 'plus_one', 'quantidade', 'Quantidade', 'ex. 1'));
+    span.appendChild(formFields(index, 's12 m9 xl2', 'account_circle', 'autor', 'Autor', 'ex: Antoine de Saint-Exupéry'));
+    span.appendChild(formFields(index, 's12 m12 xl3', 'title', 'titulo', 'Título', 'O Pequeno Príncipe'));
+    span.appendChild(formFields(index, 's12 m3 xl1', 'import_contacts', 'edicao', 'Edição', 'ex. 1ª'));
+    span.appendChild(formFields(index, 's6 m3 xl1', 'date_range', 'ano', 'Ano', 'ex: 1945'));
+    span.appendChild(formFields(index, 's12 m6 xl2', 'domain', 'editora', 'Editora', 'ex: Editora Cultura'));
+    span.appendChild(formFields(index, 's12 m12 xl2', 'place', 'local', 'Local', 'ex: Brasil'));
 }
 
- var count = 1;
+var count = 1;
